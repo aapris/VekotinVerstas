8718520204b1f032c1e2075310a65beffe5452ad
Henri Kotkanen <henri.kotkanen@gmail.com>
2017-06-01 19:02:41 +0300

    added mqtt broker for 'reading' sensly HAT

---
+++ rpi-ble-services/sensors/broker.js
@@ -0,0 +1,36 @@
+var mosca = require('mosca');
+
+var ascoltatore = {
+    type: 'redis',
+    redis: require('redis'),
+    db: 12,
+    port: 6379,
+    //return_buffers: true, // binary payloads
+    host: "localhost"
+};
+
+var moscaSettings = {
+    port: 1883,
+    backend: ascoltatore,
+    persistence: {
+        factory: mosca.persistence.Redis
+    }
+};
+
+function setup() {
+    console.log('Mosca mqtt broker up and running!');
+}
+
+function makeMQTTBroker() {
+    var server = new mosca.Server(moscaSettings);
+
+    server.on('ready', setup);
+
+    server.on('clientConnected', function(client) {
+        console.log('client connected', client.id);
+    });
+
+    return server;
+}
+
+module.exports = makeMQTTBroker;
